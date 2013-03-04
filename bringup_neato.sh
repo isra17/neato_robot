983aeb683188426a185dccd78bf7a5be9b3026f9
Israël Hallé <isra017@gmail.com>
2013-03-03 22:59:05 -0500

    Switched to ttyUSB0

--- bringup_neato.sh
+++ bringup_neato.sh
@@ -1,4 +1,4 @@
 sudo modprobe usbserial vendor=0x2108 product=0x780b
-sudo chmod go+rw /dev/ttyACM0
-rosparam set /neato/port /dev/ttyACM0
+sudo chmod go+rw /dev/ttyUSB0
+rosparam set /neato/port /dev/ttyUSB0
 roslaunch neato_slam slam.launch 
